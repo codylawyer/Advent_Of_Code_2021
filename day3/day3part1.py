c5182bd5a235ba2190b42288b3d3a84757da30b2
codylawyer <codylawyer@gmail.com>
2021-12-03 07:34:08 -0500

    day3

--- day3/day3part1.py
+++ day3/day3part1.py
@@ -1,5 +1,28 @@
 f = open('input.txt')
 line = f.readline().strip()
 
+onesCount = [0 for i in range(0,len(line))] 
+zerosCount = [0 for i in range(0,len(line))] 
+
 while line:
+    for idx,val in enumerate(line):
+        if val == '1':
+            onesCount[idx] += 1
+        else:
+            zerosCount[idx] += 1
     line = f.readline().strip()
+
+gamma = ''
+epsilon = ''
+
+for idx in range(len(onesCount)):
+    if onesCount[idx] > zerosCount[idx]:
+        gamma += '1'
+        epsilon += '0'
+    else:
+        gamma += '0'
+        epsilon += '1'
+
+gamma = int(gamma,2)
+epsilon = int(epsilon,2)
+print(gamma*epsilon)
--- day3/day3part2.py
+++ day3/day3part2.py
@@ -1,5 +1,65 @@
+import copy
+
+def countBits(values):
+    onesCount = [0 for i in range(0,len(values[0]))] 
+    zerosCount = [0 for i in range(0,len(values[0]))] 
+    for value in values:
+        for idx,val in enumerate(value):
+            if val == '1':
+                onesCount[idx] += 1
+            else:
+                zerosCount[idx] += 1
+    return onesCount,zerosCount
+
 f = open('input.txt')
 line = f.readline().strip()
 
+values = []
+
 while line:
+    values.append(line)
     line = f.readline().strip()
+
+onesCount,zerosCount = countBits(values)
+
+oxygen = '0'
+co2  = '0'
+
+oxygenValues = copy.deepcopy(values)
+co2Values = copy.deepcopy(values)
+
+for idx in range(len(onesCount)):
+    onesCount,zerosCount = countBits(oxygenValues)
+    if len(oxygenValues) > 1:
+        if onesCount[idx] > zerosCount[idx]:
+            valueToKeep = '1'
+        elif onesCount[idx] == zerosCount[idx]:
+            valueToKeep = '1'
+        else:
+            valueToKeep = '0'
+        valuesToKeep = []
+        for value in oxygenValues:
+            if value[idx]==valueToKeep:
+                valuesToKeep.append(value)
+        oxygenValues = valuesToKeep
+oxygen = oxygenValues[0]
+
+for idx in range(len(onesCount)):
+    onesCount,zerosCount = countBits(co2Values)
+    if len(co2Values) > 1:
+        if onesCount[idx] < zerosCount[idx]:
+            valueToKeep = '1'
+        elif onesCount[idx] == zerosCount[idx]:
+            valueToKeep = '0'
+        else:
+            valueToKeep = '0'
+        valuesToKeep = []
+        for value in co2Values:
+            if value[idx]==valueToKeep:
+                valuesToKeep.append(value)
+        co2Values = valuesToKeep
+co2 = co2Values[0]
+
+oxygen = int(oxygen,2)
+co2 = int(co2,2)
+print(oxygen*co2)
